4f6f002ea12c250c635fc7dc4988999ccb4d0dd9
Helitha Wijesooriya. <143673858+Helitha1@users.noreply.github.com>
2024-05-23 00:51:43 +0530

    Marketing commit

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -163,7 +163,7 @@ public class Login extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        3
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
