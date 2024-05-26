96bfaed4e6b8628ec22cb1fbfa44571adb9a93c4
damidu-prasad <damiduprasadjayarathna@gmail.com>
2024-05-26 19:43:39 +0530

    pushing to master

--- src/GUI/RoomSelection.java
+++ src/GUI/RoomSelection.java
@@ -437,6 +437,7 @@ public class RoomSelection extends javax.swing.JFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
         new RoomAvailability().setVisible(true);
+        
         this.setVisible(false);
     }//GEN-LAST:event_jButton3ActionPerformed
 
