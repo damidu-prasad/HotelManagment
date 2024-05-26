f1738f0efa2f815da0ba9ddbf023d1ca52f1dad8
lahiru  madushan <mlahiru317@gmail.com>
2024-05-26 15:57:38 +0530

    updated using 25/05/2024 6.11pm

---
+++ src/model/Customer.java
@@ -0,0 +1,197 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author mlahi
+ */
+public class Customer {
+
+    private static String Name = null;
+    private static String Email = null;
+    private static String NIC = null;
+    private static String Mobile = null;
+    private static String Religion = null;
+    private static String Country = null;
+    private static String Address = null;
+    private static String Description = null;
+    private static String ReservationNo = null;
+
+    private static String ProfilePhoto = null;
+    private static String NicFront = null;
+    private static String NicBack = null;
+
+    /**
+     * @return the Name
+     */
+    public static String getName() {
+        return Name;
+    }
+
+    /**
+     * @param aName the Name to set
+     */
+    public static void setName(String aName) {
+        Name = aName;
+    }
+
+    /**
+     * @return the Email
+     */
+    public static String getEmail() {
+        return Email;
+    }
+
+    /**
+     * @param aEmail the Email to set
+     */
+    public static void setEmail(String aEmail) {
+        Email = aEmail;
+    }
+
+    /**
+     * @return the NIC
+     */
+    public static String getNIC() {
+        return NIC;
+    }
+
+    /**
+     * @param aNIC the NIC to set
+     */
+    public static void setNIC(String aNIC) {
+        NIC = aNIC;
+    }
+
+    /**
+     * @return the Mobile
+     */
+    public static String getMobile() {
+        return Mobile;
+    }
+
+    /**
+     * @param aMobile the Mobile to set
+     */
+    public static void setMobile(String aMobile) {
+        Mobile = aMobile;
+    }
+
+    /**
+     * @return the Religion
+     */
+    public static String getReligion() {
+        return Religion;
+    }
+
+    /**
+     * @param aReligion the Religion to set
+     */
+    public static void setReligion(String aReligion) {
+        Religion = aReligion;
+    }
+
+    /**
+     * @return the Country
+     */
+    public static String getCountry() {
+        return Country;
+    }
+
+    /**
+     * @param aCountry the Country to set
+     */
+    public static void setCountry(String aCountry) {
+        Country = aCountry;
+    }
+
+    /**
+     * @return the Address
+     */
+    public static String getAddress() {
+        return Address;
+    }
+
+    /**
+     * @param aAddress the Address to set
+     */
+    public static void setAddress(String aAddress) {
+        Address = aAddress;
+    }
+
+    /**
+     * @return the Description
+     */
+    public static String getDescription() {
+        return Description;
+    }
+
+    /**
+     * @param aDescription the Description to set
+     */
+    public static void setDescription(String aDescription) {
+        Description = aDescription;
+    }
+
+    /**
+     * @return the ProfilePhoto
+     */
+    public static String getProfilePhoto() {
+        return ProfilePhoto;
+    }
+
+    /**
+     * @param aProfilePhoto the ProfilePhoto to set
+     */
+    public static void setProfilePhoto(String aProfilePhoto) {
+        ProfilePhoto = aProfilePhoto;
+    }
+
+    /**
+     * @return the NicFront
+     */
+    public static String getNicFront() {
+        return NicFront;
+    }
+
+    /**
+     * @param aNicFront the NicFront to set
+     */
+    public static void setNicFront(String aNicFront) {
+        NicFront = aNicFront;
+    }
+
+    /**
+     * @return the NicBack
+     */
+    public static String getNicBack() {
+        return NicBack;
+    }
+
+    /**
+     * @param aNicBack the NicBack to set
+     */
+    public static void setNicBack(String aNicBack) {
+        NicBack = aNicBack;
+    }
+
+    /**
+     * @return the ReservationNo
+     */
+    public static String getReservationNo() {
+        return ReservationNo;
+    }
+
+    /**
+     * @param aReservationNo the ReservationNo to set
+     */
+    public static void setReservationNo(String aReservationNo) {
+        ReservationNo = aReservationNo;
+    }
+    
+    
+
+}
